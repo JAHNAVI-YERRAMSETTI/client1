b3789371684f899699e053aac2b0f779e083c3e9
JAHNAVI-YERRAMSETTI <2300031078@kluniversity.in>
2024-11-14 16:50:30 +0530

    Update FacultyList.js

--- src/components/FacultyList.js
+++ src/components/FacultyList.js
@@ -9,7 +9,7 @@ function FacultyList() {
   const [position, setPosition] = useState('');
   const [editingId, setEditingId] = useState(null);
 
-  const API_URL = `https://server1-1-jr4d.onrender.com`;
+  const API_URL = `https://server1-1-jr4d.onrender.com/api/faculty`;
 
   useEffect(() => {
     fetchFaculty();
